7c38d19276980b05ac6248adf3f92ebf0d4cbf53
Tanveer Munir <tanveer.munir@jeeny.me>
2022-08-16 00:17:23 +0500

    Get categories repo implementation added

--- app/src/main/java/com/task/channelapp/domain/DataRepository.kt
+++ app/src/main/java/com/task/channelapp/domain/DataRepository.kt
@@ -1,9 +1,15 @@
 package com.task.channelapp.domain
 
+import com.task.channelapp.data.local.entities.CategoryEntity
 import com.task.channelapp.data.local.localservice.ChannelDbService
+import com.task.channelapp.data.remote.baseclient.ApiResponse
+import com.task.channelapp.data.remote.baseclient.models.BaseResponse
+import com.task.channelapp.data.remote.responsedtos.CategoryResponse
 import com.task.channelapp.data.remote.services.channelservice.ChannelApi
+import com.task.channelapp.domain.base.DataError
 import com.task.channelapp.domain.base.DataResponse
 import com.task.channelapp.domain.dtos.CategoryDTO
+import com.task.channelapp.domain.dtos.CategoryData
 import com.task.channelapp.domain.interfaces.ICategoryDataRepo
 import com.task.channelapp.domain.interfaces.IChannelDataRepo
 import com.task.channelapp.domain.interfaces.IEpisodeDataRepo
@@ -15,6 +21,40 @@ class DataRepository @Inject constructor(
 ) : ICategoryDataRepo, IChannelDataRepo, IEpisodeDataRepo {
 
     override suspend fun getAllCategories(isRefresh: Boolean): DataResponse<CategoryDTO> {
-        TODO("Not yet implemented")
+        val categories = localRepository.getCategories()
+        return when {
+            categories?.isNotEmpty() == true && !isRefresh -> {
+                val currencyList = categories.map { entity ->
+                    CategoryData(
+                        name = entity.name
+                    )
+                }
+                DataResponse.Success(data = CategoryDTO(currencyList))
+            }
+            else -> {
+                when (val response: ApiResponse<BaseResponse<CategoryResponse>> =
+                    remoteRepository.fetchCategories()) {
+                    is ApiResponse.Success -> {
+                        val categoryList =
+                            response.data.data?.categories?.map { CategoryData(it.name ?: "") }
+                                ?: listOf()
+                        localRepository.insertCategories(categoryList.map { category ->
+                            CategoryEntity(
+                                name = category.name
+                            )
+                        })
+                        DataResponse.Success(CategoryDTO(categoryList))
+                    }
+                    is ApiResponse.Error -> {
+                        DataResponse.Error(
+                            DataError(
+                                response.error.statusCode,
+                                response.error.message
+                            )
+                        )
+                    }
+                }
+            }
+        }
     }
 }
